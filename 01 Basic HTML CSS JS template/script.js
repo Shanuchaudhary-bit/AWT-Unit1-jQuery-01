b209326440b5e7fe0056191d50097fb9e77d93e8
prateekrajgautam <prateekrajgautam@gmail.com>
2025-08-23 15:55:46 +0530

    init2

--- 01 Basic HTML CSS JS template/script.js
+++ 01 Basic HTML CSS JS template/script.js
@@ -1,2 +1,28 @@
 console.clear();
-console.log("Hello World! from script.js");
+console.info("Hello World! from script.js");
+
+// var elem = document.getElementById("btn");
+// elem.addEventListener("click", function() {
+//   alert("Button clicked!");
+//   var ptag = document.querySelectorAll("p");
+//   ptag[0].style.color = "red";
+// });
+
+
+// $("#btn").on("click", function() {
+//     alert("Button clicked!");
+//     $("p").eq(0).css("color", "red");
+//     $("#btn").html("Clicked");
+// });
+
+
+$("button").on("click",buttonevent);
+
+
+
+function buttonevent(){
+    var buttonContent = this.innerHTML
+    alert(`${buttonContent} is clicked!`);
+    this.style.color = "red";
+    
+}
